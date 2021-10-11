85cf54d497bca567e3809564b55c85321edb808e
GabrielVRV <GabrielVrv1910@gmail.com>
2021-10-11 18:24:02 -0300

    Changed the script to be compatible with the interface

--- processing/register.py
+++ processing/register.py
@@ -1,29 +1,28 @@
-import cv2
+def registrar(personName):
+  import cv2
 
-personName = input('Digite o nome: ')
+  if personName:
+    cap = cv2.VideoCapture(0)
 
-if personName:
-  cap = cv2.VideoCapture(0)
+    while True:
+      success, img = cap.read()
 
-  while True:
-    success, img = cap.read()
+      if not success:
+        print("couldn't grab the frame")
+        break
 
-    if not success:
-      print("couldn't grab the frame")
-      break
+      cv2.imshow('Register', img)
 
-    cv2.imshow('Register', img)
+      pressedKey = cv2.waitKey(1)
 
-    pressedKey = cv2.waitKey(1)
+      if pressedKey == ord(' '):
+        img_name = f'{personName}.jpg'
+    
+        cv2.imwrite(f'./processing/resources/img/attendance/{img_name}', img)
 
-    if pressedKey == ord(' '):
-      img_name = f'{personName}.jpg'
-  
-      cv2.imwrite(f'./processing/resources/img/attendance/{img_name}', img)
+        print('photo saved')
 
-      print('photo saved')
+        break
 
-      break
-
-    elif pressedKey == ord('q'):
-      break
+      elif pressedKey == ord('q'):
+        break
