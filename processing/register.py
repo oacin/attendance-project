a4486ce011c07a0fabf4d992502fca2c3522ccc7
GabrielVRV <GabrielVrv1910@gmail.com>
2021-10-15 20:02:49 -0300

    Improvement in the registration script, now it only register if there is only one face in the camera.

--- processing/register.py
+++ processing/register.py
@@ -1,5 +1,7 @@
 def registrar(personName):
   import cv2
+  import os
+  classificador = cv2.CascadeClassifier('./processing/haarcascade_frontalface_default.xml')
 
   cap = cv2.VideoCapture(0)
 
@@ -19,9 +21,19 @@ def registrar(personName):
     
       cv2.imwrite(f'./processing/resources/img/attendance/{img_name}', img)
 
-      print('photo saved')
+      imagem = cv2.imread(f'./processing/resources/img/attendance/{img_name}')
+      imagemcinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-      break
+      facesdetectadas = classificador.detectMultiScale(imagemcinza,scaleFactor=1.1, minNeighbors=8,minSize=(30,30))
+
+      if len(facesdetectadas) == 1:
+        break
+      elif len(facesdetectadas) > 1:
+        print('Muitas pessoas')
+        os.remove(f'./processing/resources/img/attendance/{img_name}')
+      else:
+        print('Não tem ninguem')
+        os.remove(f'./processing/resources/img/attendance/{img_name}')
 
     elif pressedKey == ord('q'):
       break
