73d7ba25f6c27616e1d6295e9b2299506a175793
GabrielVRV <GabrielVrv1910@gmail.com>
2021-10-15 19:06:41 -0300

    Fixed interface size, previously components were too small

--- interface/register_menu.py
+++ interface/register_menu.py
@@ -18,7 +18,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.setWindowModality(QtCore.Qt.WindowModal)
-        MainWindow.resize(432, 393)
+        MainWindow.resize(800, 600)
         MainWindow.setAutoFillBackground(False)
         MainWindow.setStyleSheet("background-color: rgb(0, 0, 0);")
         MainWindow.setIconSize(QtCore.QSize(20, 20))
@@ -28,7 +28,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.backgroundAttendance = QtWidgets.QFrame(self.centralwidget)
-        self.backgroundAttendance.setMaximumSize(QtCore.QSize(425, 400))
+        self.backgroundAttendance.setMaximumSize(QtCore.QSize(1920, 1080))
         self.backgroundAttendance.setStyleSheet("color: rgb(0, 0, 0);")
         self.backgroundAttendance.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.backgroundAttendance.setFrameShadow(QtWidgets.QFrame.Raised)
@@ -36,7 +36,7 @@ class Ui_MainWindow(object):
         self.verticalLayout = QtWidgets.QVBoxLayout(self.backgroundAttendance)
         self.verticalLayout.setObjectName("verticalLayout")
         self.frame = QtWidgets.QFrame(self.backgroundAttendance)
-        self.frame.setMaximumSize(QtCore.QSize(500, 90))
+        self.frame.setMaximumSize(QtCore.QSize(1920, 186))
         self.frame.setStyleSheet("image: url(:/Imagens/images/attendance project.png);")
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
@@ -58,13 +58,13 @@ class Ui_MainWindow(object):
         self.namePerson.setObjectName("namePerson")
         self.verticalLayout.addWidget(self.namePerson)
         self.buttonConfirm = QtWidgets.QPushButton(self.backgroundAttendance)
-        self.buttonConfirm.setMinimumSize(QtCore.QSize(200, 60))
+        self.buttonConfirm.setMinimumSize(QtCore.QSize(200, 115))
         self.buttonConfirm.setStyleSheet("image: url(:/Buttons/images/btnConfirmar.png);")
         self.buttonConfirm.setText("")
         self.buttonConfirm.setObjectName("buttonConfirm")
         self.verticalLayout.addWidget(self.buttonConfirm)
         self.buttonCancel = QtWidgets.QPushButton(self.backgroundAttendance)
-        self.buttonCancel.setMinimumSize(QtCore.QSize(200, 60))
+        self.buttonCancel.setMinimumSize(QtCore.QSize(200, 115))
         self.buttonCancel.setStyleSheet("image: url(:/Buttons/images/btnCancelar.png);")
         self.buttonCancel.setText("")
         self.buttonCancel.setObjectName("buttonCancel")
@@ -85,6 +85,10 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Attendance Project"))
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
