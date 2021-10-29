696f1fd4afdaa7b1ca4b56de0fdad847d4a81357
GabrielVRV <GabrielVrv1910@gmail.com>
2021-10-29 19:36:28 -0300

    Added new style to LineEdit

--- interface/register_menu.py
+++ interface/register_menu.py
@@ -10,6 +10,38 @@ sys.path.insert(1, './processing')
 import register
 
 class Ui_MainWindow(object):
+
+    #Estilos da linha
+    styleLineEditOk = ("QLineEdit {\n"
+"    border: 2px solid rgb(45, 45, 45);\n"
+"    border-radius: 5px;\n"
+"    padding: 15px;\n"
+"    background-color: rgb(30, 30, 30);    \n"
+"    color: rgb(100, 100, 100);\n"
+"}\n"
+"QLineEdit:hover {\n"
+"    border: 2px solid rgb(55, 55, 55);\n"
+"}\n"
+"QLineEdit:focus {\n"
+"    border: 2px solid rgb(255, 207, 0);    \n"
+"    color: rgb(200, 200, 200);\n"
+"}")
+
+    styleLineEditError = ("QLineEdit {\n"
+"    border: 2px solid rgb(255, 0, 0);\n"
+"    border-radius: 5px;\n"
+"    padding: 15px;\n"
+"    background-color: rgb(30, 30, 30);    \n"
+"    color: rgb(100, 100, 100);\n"
+"}\n"
+"QLineEdit:hover {\n"
+"    border: 2px solid rgb(55, 55, 55);\n"
+"}\n"
+"QLineEdit:focus {\n"
+"    border: 2px solid rgb(255, 207, 0);    \n"
+"    color: rgb(200, 200, 200);\n"
+"}")
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.setWindowModality(QtCore.Qt.WindowModal)
@@ -38,18 +70,14 @@ class Ui_MainWindow(object):
         self.frame.setObjectName("frame")
         self.verticalLayout.addWidget(self.frame)
         self.namePerson = QtWidgets.QLineEdit(self.backgroundAttendance)
-        self.namePerson.setEnabled(True)
+        self.namePerson.setGeometry(QtCore.QRect(85, 288, 280, 50))
+
+        self.namePerson.setStyleSheet(self.styleLineEditOk)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.namePerson.sizePolicy().hasHeightForWidth())
-        self.namePerson.setSizePolicy(sizePolicy)
-        self.namePerson.setMinimumSize(QtCore.QSize(201, 19))
-        self.namePerson.setMaximumSize(QtCore.QSize(16777215, 16777215))
-        self.namePerson.setCursor(QtGui.QCursor(QtCore.Qt.IBeamCursor))
-        self.namePerson.setStyleSheet("color: rgb(255, 255, 255);\n"
-"background-color: rgb(112, 112, 112);")
-        self.namePerson.setText("")
+        self.namePerson.setMaxLength(32)
         self.namePerson.setObjectName("namePerson")
         self.verticalLayout.addWidget(self.namePerson)
         self.buttonConfirm = QtWidgets.QPushButton(self.backgroundAttendance)
@@ -127,6 +155,7 @@ class Ui_MainWindow(object):
             msg.setInformativeText('É necessário digitar o nome completo da pessoa para que consiga registra-la.')
             msg.setWindowTitle("Campo não preenchido")
             msg.exec_()
+            self.namePerson.setStyleSheet(self.styleLineEditError)
             return
 
         sys.exit(app.exec_())
